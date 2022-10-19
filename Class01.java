d3c3681ba5e4a40eed071c2f6eab6a8074d0b438
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2022-10-19 11:56:26 +0800

    Create Class01.java

---
+++ Class01.java
@@ -0,0 +1,38 @@
+package test1019;
+class CRectangle
+{
+	int width;
+	int height;
+	public CRectangle(int w,int h)
+	{
+		width=w;
+		height=h;
+	}
+	public CRectangle() //沒有引數的建構元  (a)
+	{
+		width=10;
+		height=8;
+	}
+	//	public CRectangle() //沒有引數的建構元  (b)
+	//{
+		//this(10,8);
+		//int width;
+		//int height;
+	//}
+	
+	public void show()
+	{
+		System.out.println("width="+width);
+		System.out.println("height="+height);
+		
+	}
+
+}
+
+public class Class01 {
+	public static void main(String args[])
+	{
+		CRectangle c1=new CRectangle();
+		c1.show();
+	}
+}

6281e1a7eeba636071fe0b99b8906b05b5cdc8fd
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2022-10-25 17:19:09 +0800

    Add files via upload

---
+++ Class07.java
@@ -0,0 +1,15 @@
+class C
+{
+ public static void power(int i,int j)
+ {
+  System.out.println(Math.pow(i, j)); //次方   
+ }
+}
+
+public class Class07 {
+    public static void main(String args[])
+    {
+     C.power(2, 5);
+     C.power(3, 2);
+    }
+}
